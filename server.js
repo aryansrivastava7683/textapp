d4c3d76920dfea7b8da8d3236dbb861896620c31
Night <96472381+aryansrivastava7683@users.noreply.github.com>
2024-01-13 15:42:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,22 +6,25 @@ const chatInputForm = document.querySelector('.chat-input'); // chat input form
 const chatInput = document.querySelector('.chatInput'); // chat input
 const clearbtn = document.querySelector('.clear-chat'); // clear button form
 
+
 let messages = JSON.parse(localStorage.getItem('messages')) || [];
 
-// Ensure messages is an array
-if (!Array.isArray(messages)) {
-  messages = [];
-}
 
-const person1 = 'person1';
-const person2 = 'person2';
+
+const person1 = 'Person1';
+const person2 = 'Person2';
 
 const chatMessageElement = (message) => `
+
   <div class="message ${message.sender === person1 ? 'bg-blue' : 'bg-grey'}">
-    <div class="message-sender">${message.text}</div>
+    <div class="message-sender">${message.sender}</div>
+    <div class="message-text">${message.text}</div>
     <div class="message-timestamp">${message.timestamp}</div>
   </div>
-`;
+
+
+
+  `;
 
 window.onload = () => {
   messages.forEach((message) => {
@@ -29,18 +32,19 @@ window.onload = () => {
   });
 };
 
-let messageSender = 'person1';
+let messageSender = person1;
 const updateMessageSender = (name) => {
   messageSender = name;
   chatHeader.innerText = `${messageSender} chatting...`;
+  
   chatInput.placeholder = `Type here, ${messageSender}`;
 
-  if (name === 'person1') {
+  if (name === person1) {
     person1select.classList.add('active-role');
     person2select.classList.remove('active-role');
   }
 
-  if (name === 'person2') {
+  if (name === person2) {
     person2select.classList.add('active-role');
     person1select.classList.remove('active-role');
   }
@@ -56,10 +60,12 @@ const sendMessage = (e) => {
 
   const timestamp = new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
   const message = {
+    
     sender: messageSender,
     text: chatInput.value,
     timestamp,
   };
+  
 
   messages.push(message);
   localStorage.setItem('messages', JSON.stringify(messages));
